ac391f441bb7e6c1106563ad25451e5db8434af5
dcastagn <112878659+DarioCastagnola@users.noreply.github.com>
2023-03-10 15:02:44 +0100

    Add files via upload

---
+++ so_long/main.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastagn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 16:45:14 by dcastagn          #+#    #+#             */
+/*   Updated: 2023/03/10 11:21:50 by dcastagn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	main(int argc, char **argv)
+{
+	t_game		game;
+
+	if (argc != 2)
+		null_error("Wrong Input!!");
+	game.end = 0;
+	game.player_moves = 0;
+	game.mlx = mlx_init();
+	game.map = read_map(argv[1]);
+	is_valid_map(game.map);
+	game.mlx_win = mlx_new_window(game.mlx,
+			ft_strlen_file(argv[1]) * 64,
+			file_linecount(argv[1]) * 64, "so_long");
+	mlx_key_hook(game.mlx_win, key_hook, &game);
+	mlx_hook(game.mlx_win, 17, 0, ft_destroy_window, &game);
+	img_init(&game);
+	draw_map(&game);
+	print_moves(&game);
+	mlx_loop(game.mlx);
+}
+
+int	ft_destroy_window(t_game *game)
+{
+	mlx_destroy_window(game->mlx, game->mlx_win);
+	exit(ft_printf("You left the Summoner's Rift!\n"));
+}
