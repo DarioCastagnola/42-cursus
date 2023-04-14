3bdbf58d7aea4fb65ae597af086cce514b088a6c
dcastagn42 <112878659+DarioCastagnola@users.noreply.github.com>
2023-04-14 11:32:03 +0200

    Add files via upload

---
+++ minitalk/minitalk_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastagn <dcastagn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 11:23:33 by dcastagn          #+#    #+#             */
+/*   Updated: 2023/04/14 11:23:51 by dcastagn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "ft_printf/ft_printf.h"
+
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define END "\033[0m"
+
+#endif
